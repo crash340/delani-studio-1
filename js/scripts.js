cfce401cb0c2299228d790106bd4acbc3afaf67c
Robert Muthuri <muthuri.r@gmail.com>
2019-07-22 12:33:32 +0300

    Adds hover functions over the portfolio icons.

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,28 @@ $(function(){
 		$("#product-image").toggle();
 		$("#product-text").toggle();
 	});
+	$("div#jim").hover(function() {
+		$("div#jim-text").toggle();
+	})
+	$("div#bailey").hover(function() {
+		$("div#bailey-text").toggle();
+	});
+	$("div#burned").hover(function(){
+		$("div#burned-text").toggle();
+	});
+	$("div#age").hover(function(){
+		$("div#age-text").toggle();
+	});
+	$("div#elexa").hover(function(){
+		$("div#elexa-text").toggle();
+	});
+	$("div#crystals").hover(function(){
+		$("div#crystals-text").toggle();
+	});
+	$("div#ontario").hover(function(){
+		$("div#ontario-text").toggle();
+	});
+	$("div#dark").hover(function(){
+		$("div#dark-text").toggle();
+	});
 });
